cd6ca93e7887a11292d2efabb438240ffcf5cb73
Jason Wong <jaw.wong@mail.utoronto.ca>
2016-05-19 04:16:37 -0400

    Initial commit of python file

---
+++ sample.py
@@ -0,0 +1,71 @@
+from mongoengine import *
+import datetime
+
+DISCIPLINES = ('Chemical', 'Civil', 'Electrical', 'Industrial', 'Mechanical', 'Materials')
+ACADEMIC_STATUS = ('registered', 'withdrawn', 'probation')
+
+def printStudentInfo(Student):
+	print ("%s %s %d" % (Student.name, Student.discipline, Student.year)) 
+	return
+
+class CustomQuerySet(QuerySet):
+	def filter_by_year(self, year_):
+		return self.filter(year= year_)
+
+	def is_graduation_elliglble(self):
+		return self.filter(
+			Q(academicInfo__peyCompleted= True) | Q(academicInfo__hoursCompleted= True) 
+		)
+
+def main():
+	connect('importedDB')
+
+	deansList = Student.objects()
+	for student in deansList:
+		printStudentInfo(student)
+
+
+class AcademicInfo(EmbeddedDocument):
+	registered = DateTimeField()
+	status  = StringField( max_length=20, choices = ACADEMIC_STATUS )
+	peyCompleted = BooleanField()
+	hoursCompleted = BooleanField()
+	graduationYear = IntField( min_value = 2016, max_value = 2019)
+	gpa = ListField(
+		DecimalField(min_value=0, max_value=4.0) )
+
+class RegistrationInfo(EmbeddedDocument):
+	paid = BooleanField()
+	international = BooleanField()
+
+class ContactInfo(EmbeddedDocument):
+	email = EmailField(max_length = 50)
+	phone = StringField(max_lenght = 9)
+
+
+class Student(Document):
+	name = StringField(max_length=50, required=True )
+	year = IntField(min_value = 1, max_value = 4)
+	discipline = StringField( max_length=20, choices= DISCIPLINES)
+
+	#EmbeddedDocuments 
+	registrationInfo = EmbeddedDocumentField(RegistrationInfo)
+	contactInfo = EmbeddedDocumentField(ContactInfo)
+	academicInfo = EmbeddedDocumentField(AcademicInfo)	
+
+	meta = {
+		'collection': 'students',
+		'queryset_class': CustomQuerySet
+	}
+
+
+def isDeansList(student):
+	studentGPA = student.academicInfo['gpa']
+	meanGPA = sum(studentGPA)/len(studentGPA)
+	numDeansList = 0
+	if meanGPA >= 2.7:
+		numDeansList += 1
+	print ("%.2f %d" % (meanGPA, numDeansList) )
+
+if __name__ == "__main__":
+	main()
